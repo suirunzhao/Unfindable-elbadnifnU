b52100963a949973677f2434ca1def8e163d8b70
suirunzhao <102000475+suirunzhao@users.noreply.github.com>
2022-05-13 17:02:16 -0400

    Update ServoTest.ino

--- Code(Arduino)/ServoTest.ino
+++ Code(Arduino)/ServoTest.ino
@@ -1,43 +1,52 @@
-import mediapipe as mp
-import numpy as np
-import cv2
-import serial
-import sys
-import time
-
-#Setup Communication path for arduino
-arduino = serial.Serial(port='/dev/cu.usbmodem14401', baudrate=9600)
-time.sleep(2) #change the port(14401) showing in your Arduino IDE
-print("Connected to Arduino...")
+#include<Servo.h>
 
 
-cap = cv2.VideoCapture(0) #When you connect to web camera chage 0 to 1 
-cap.set(3,1280) #display size
-cap.set(4,720)
+Servo servoHor; //Horizontal Servo
 
-facemesh = mp.solutions.face_mesh
-face = facemesh.FaceMesh(static_image_mode=True, min_tracking_confidence=0.6, min_detection_confidence=0.6)
-draw = mp.solutions.drawing_utils
+int x;
 
-while True:
 
-	_, me = cap.read()
+int prevX;
 
-	rgb = cv2.cvtColor(me, cv2.COLOR_BGR2RGB)
 
-	op = face.process(rgb)
-	if op.multi_face_landmarks:
-		for i in op.multi_face_landmarks:
-			print(i.landmark[0].x*1280)
-			xx = int(i.landmark[0].x*1280)
-			print(xx)
-			# sending data to arduino
-			data = "X{0:d}YZ".format(xx)
-			arduino.write(data.encode())
+void setup()
+{
+  Serial.begin(9600);
 
-			
-	cv2.imshow("window", cv2.flip(me,1)) #you can delete this if you do not want to show
-	if cv2.waitKey(1) == 27:
-		cap.release()
-		cv2.destroyAllWindows()
-		break
+  servoHor.attach(6); //Attach Horizontal Servo to Pin 6
+
+  servoHor.write(90);
+}
+
+void Pos()
+{
+  if(prevX != x)
+  {
+    //generate map
+    int servoX = map(x, 0, 1280, 10, 179); 
+
+
+    servoX = min(servoX, 179);
+    servoX = max(servoX, 10);
+
+    
+    servoHor.write(servoX);
+
+  }
+}
+
+void loop()
+{
+  if(Serial.available() > 0)
+  {
+    if(Serial.read() == 'X')
+    {
+      x = Serial.parseInt();
+       Pos();
+      }
+    }
+    while(Serial.available() > 0)
+    {
+      Serial.read();
+    }
+  }
